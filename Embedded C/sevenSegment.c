45de52c601db2d8781202675e14ba04a5e7a5a14
Shreyas <93028386+shr-eyas@users.noreply.github.com>
2023-05-28 01:47:48 +0530

    Add files via upload

---
+++ Embedded C/sevenSegment.c
@@ -0,0 +1,27 @@
+#include <avr/io.h>
+ 
+int main (void)
+{
+	
+  DDRD |= 0xFC;               //set PD2-PD7 as output pins 0xFC=0b11111100 (binary)
+  DDRB |= ((1 << DDB0));      //set PB0 as output pin
+
+  while (1) {
+
+    PORTB = ((1 <<  PB0));    //turn PB0 off (pin 8)
+
+    // PD2 corresponds to digital pin 2
+    // PD3 corresponds to digital pin 3
+    // PD4 corresponds to digital pin 4
+    // PD5 corresponds to digital pin 5
+    // PD6 corresponds to digital pin 6
+    // PD7 corresponds to digital pin 7 
+
+    PORTD = 0b11100100;        
+            //76543201
+  }
+
+
+  return 0;
+
+}
